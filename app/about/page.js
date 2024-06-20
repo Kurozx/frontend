32de9831e2565e92ddb8e10b309203bbe3862dde
Kurozx <natchanondokchan@gmail.com>
2024-06-20 13:27:12 +0700

    change about

--- app/about/page.js
+++ app/about/page.js
@@ -1,17 +1,9 @@
-import Navbar from '../app/component/nav';
+import Navbar from '/app/component/nav';
 
-export default function about() {
+export default function Home() {
   return (
     <>
       <Navbar />
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="..."></img>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
     </>
   );
 }
