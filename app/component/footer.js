ed4649461c9c6e4c7d4486274d7db20cbd7c4ca1
Kurozx <natchanondokchan@gmail.com>
2024-08-08 12:15:57 +0700

    8/8/2567

--- app/component/footer.js
+++ app/component/footer.js
@@ -7,7 +7,7 @@ export default function footer() {
     <footer className="py-5" class="container text-align">
         <div className="row">
           <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
+            <h5>เว็ปไซต์</h5>
             <ul className="nav flex-column">
               <li className="nav-item mb-2"><Link href="/" className="nav-link p-0 text-muted">Home</Link></li>
               <li className="nav-item mb-2"><Link href="/about" className="nav-link p-0 text-muted">About</Link></li>
@@ -18,8 +18,8 @@ export default function footer() {
 
           <div className="col-md-5 offset-md-1 mb-3">
             <form>
-              <h5>Subscribe to our newsletter</h5>
-              <p>Monthly digest of what's new and exciting from us.</p>
+              <h5>ติดตามข่าวสาร</h5>
+              <p>กรอกอีเมลเพื่อรับข่าวสารและสินค้าใหม่กับเรา.</p>
               <div className="d-flex flex-column flex-sm-row w-100 gap-2">
                 <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
                 <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
